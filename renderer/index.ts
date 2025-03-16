4c28f7eb891b853ce8ae654d75d26475971d1bf7
Lia-labs <liascb15@gmail.com>
2025-03-16 02:07:33 +0100

    turning the db in to a table

--- renderer/index.ts
+++ renderer/index.ts
@@ -1,5 +1,7 @@
 import "./index.scss";
 
+const table = document.getElementById("table");
+
 //console.log(window.preload);
 window.preload.on("DBData", (data : string)=>{
     console.log("receiving DBData");
@@ -9,8 +11,30 @@ window.preload.on("DBData", (data : string)=>{
 //window.preload.on("runQuery",)
 
 window.preload.start();
+
 /*let result= await window.preload.runQuery("INSERT INTO NewTable (name, age) VALUES (?, ?);", ["Jane", null]);
 console.log(result);*/
-let result= await window.preload.getData("SELECT * FROM NewTable WHERE name = ?", ["Jane"]);
-console.log(result);
+
+let consult= await window.preload.getData("SELECT * FROM NewTable");
+//console.log(consult);
+//console.log(consult["result"][3]["name"]);
+
+
+Object.entries(consult["result"]).forEach(([user, details]) => {
+
+    let row = "<tr>";
+
+    //console.log(user, details);
+
+    Object.entries(details).forEach(([key, value]) => {
+      row += `<td> ${value} </td>`;
+      //console.log(`<td> ${value} </td>`);
+    });
+
+    row += "</tr>";
+
+    table.innerHTML += row;
+  });
+
+
 console.log("It's theorically done");
